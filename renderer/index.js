667911efa12b65604411936ccbe772aa0e6c9364
James Earle <jaearle@microsoft.com>
2017-09-19 14:01:11 -0700

    request now uses promises

--- renderer/index.js
+++ renderer/index.js
@@ -2,25 +2,32 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const request = require("request");
 let key = "ADOUOEB4TRXN6KA5";
-// works but bad
-// declare var Promise: any;
 function submit() {
     let symbol = document.getElementById('add-symbol').value;
     let func = document.getElementById('function').value;
     // todo: allow for CSV download
     let uri = "https://www.alphavantage.co/query?function=" + func + "&symbol=" + symbol + "&apikey=" + key;
     let p = new Promise((resolve, reject) => {
-        resolve("heya");
-        return 123;
+        request.get(uri, (err, res, body) => {
+            if (!err && res.statusCode == 200) {
+                resolve(body);
+            }
+            else {
+                reject(err);
+            }
+        });
     });
-    let x = request.get(uri, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            appendToDom(body);
-        }
-        else {
-            return "Something went wrong...\n" + err;
-        }
+    p.then(result => {
+        appendToDom(result);
+    }).catch(error => {
+        throw error;
     });
+    // let x = request.get(uri, (err, res, body) => {
+    //     if (!err && res.statusCode == 200) {
+    //     } else {
+    //         return "Something went wrong...\n" + err; 
+    //     }
+    // });
 }
 exports.submit = submit;
 function appendToDom(arg) {
--- renderer/index.ts
+++ renderer/index.ts
@@ -2,9 +2,6 @@ import * as request from 'request';
 
 let key = "ADOUOEB4TRXN6KA5";
 
-// works but bad
-// declare var Promise: any;
-
 export function submit() {
     let symbol = (document.getElementById('add-symbol') as HTMLInputElement).value;
     let func = (document.getElementById('function') as HTMLSelectElement).value;
@@ -13,16 +10,19 @@ export function submit() {
     let uri = "https://www.alphavantage.co/query?function=" + func + "&symbol=" + symbol + "&apikey=" + key;
 
     let p = new Promise((resolve, reject) => {
-        resolve("heya");
-        return 123;
+        request.get(uri, (err, res, body) => {
+            if(!err && res.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject(err);
+            }
+        });
     });
-
-    let x = request.get(uri, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            appendToDom(body);
-        } else {
-            return "Something went wrong...\n" + err; 
-        }
+    
+    p.then(result => {
+        appendToDom(result);
+    }).catch(error => {
+        throw error;
     });
 }
 
